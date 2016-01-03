254aec4ffb179bce862a0b32aa3e915954374b8a
Ximing Chen <ximing@ximing-477.local>
2016-01-03 16:05:11 -0400

    add button

--- mac-test/mac-test/AppDelegate.m
+++ mac-test/mac-test/AppDelegate.m
@@ -11,12 +11,22 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+
+@property (nonatomic,strong) NSViewController *masterViewController;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    NSLog(@"haha");
+    NSButton *button = [[NSButton alloc] initWithFrame:CGRectMake(0,0,100,100)];
+    [self.window.contentView addSubview:button];
+     [button setAction:@selector(buttonPressed)];
     // Insert code here to initialize your application
+}
+
+-(void)buttonPressed {
+    NSLog(@"Button pressed!");
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
